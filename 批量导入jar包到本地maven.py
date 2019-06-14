cf57fc99b533534e13b2754b4e6878f84ff9d674
JameKing2018 <43434677+JameKing2018@users.noreply.github.com>
2019-06-14 17:45:58 +0800

    Create 批量导入jar包到本地maven.py

---
+++ 批量导入jar包到本地maven.py
@@ -0,0 +1,27 @@
+import os
+
+# 批量引入jar包到本地Maven仓库
+
+
+# 本地存放jar包路径
+path='D:/soft/bcjy/svn/ziyuanku/mylib'
+
+# 获取文件列表
+files=os.listdir(path)
+
+# 遍历文件列表
+for  file in files:
+    # 文件名
+    filename=file
+    # 包名
+    jarName=file.split('-')[0]
+    # groupId
+    groupId='bcjy.tjj'
+    # artifactId
+    artifactId=jarName
+    # cmd命令
+    command="mvn install:install-file -Dfile="+path+"/"+filename+" -DgroupId="+groupId+" -DartifactId="+artifactId+" -Dversion=1.0.0 -Dpackaging=jar"
+
+    print(command)
+    # 执行命令
+    os.system(command)
